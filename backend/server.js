18118e8323db390f31302ca56520b67c51f4025a
therealkratika <therealkratikagupta@gmail.com>
2026-08-22 22:39:07 +0530

    frontend deployed link added.

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ connectDB();
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://bookloom-psi.vercel.app/",
   credentials: true
 }));
 app.use(express.json());
