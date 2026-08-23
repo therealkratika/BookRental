4517fd383b484a687f19387b949871d6f26eed0b
therealkratika <therealkratikagupta@gmail.com>
2026-08-23 11:59:38 +0530

    feat: fixing 401 error

--- backend/src/model/user.js
+++ backend/src/model/user.js
@@ -50,8 +50,6 @@ userSchema.pre("save", async function (next) {
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-
-  next();
 });
 
 userSchema.methods.comparePassword = async function (enteredPassword) {
